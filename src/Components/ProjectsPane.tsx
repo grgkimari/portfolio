d1887a2ddb3825bcfd23364bf4a0fcaef6610fc3
George Kimari <grgkimari@gmail.com>
2023-02-18 14:29:40 +0300

    Updated project descriptions

--- src/Components/ProjectsPane.tsx
+++ src/Components/ProjectsPane.tsx
@@ -27,30 +27,35 @@ const ProjectsPane = () => {
         name="Password generator"
         link="https://grgkimari.github.io/password-guru"
         imagelink="password-570317_960_720.jpg"
-        description="An online strong password generator and strength checker built with React JS"
+        description="An online strong password generator and strength checker built with React JS."
       /></Grid>
       <Grid item xs={6} sm={4} md={3}><ProjectRef
         name="Quote machine"
         link="https://grgkimari.github.io/quote-machine"
         imagelink='quotes-7669023_960_720.png'
+        description="An online random inspirational quote machine created with React JS and redux."
       /></Grid>
       <Grid item xs={6} sm={4} md={3}><ProjectRef
         name="React calculator"
+        description="A nimble web based calculator built with React JS."
         imagelink="calculator-168360_960_720.jpg"
         link="https://grgkimari.github.io/nimble-calculator"
       /></Grid>
       <Grid item xs={6} sm={4} md={3}><ProjectRef
         name="Markdown Previewer"
+        description="An web based live markdown previewer built with React JS."
         link="https://grgkimari.github.io/markdown-previewer"
         imagelink="markdown-here.png"
       /></Grid>
       <Grid item xs={6} sm={4} md={3}><ProjectRef
         name="Pomodoro Timer"
+        description="An online pomodoro timer with sound notifications built with React JS."
         link="https://grgkimari.github.io/pomodoro-timer-v2"
         imagelink='stopwatch-5153023_960_720.jpg'
       /></Grid>
       <Grid item xs={6} sm={4} md={3}><ProjectRef
         name="Beat Maker"
+        description="A drum style online beat maker built with React JS."
         link="https://grgkimari.github.io/special-disco"
         imagelink="drums-3109364_960_720.jpg"
       /></Grid>
